28440687964765fadcfabda351a52d2006e08d6b
Simran Yadav <simranyadav464@gmail.com>
2024-02-02 19:47:16 +0530

    Added the file

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new typed(".text", {
+    String: ["Backend Developer", "UI/UX Designer", "Content Writer"],
+    typeSpeed: 100,
+    backSpreed: 100,
+    backDelay: 1000,
+    loop:true
+});
